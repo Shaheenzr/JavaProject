142cab5b2d75c69eb7fe39c1ea3fac1dc289712f
Shaheen nazar <101840286+Shaheenzr@users.noreply.github.com>
2022-04-25 13:49:46 -0700

    Add files via upload

---
+++ JavaPractice/programs/Language.java
@@ -0,0 +1,37 @@
+public class Language{
+
+protected String name;
+protected int numSpeakers;
+protected String regionsSpoken;
+protected String wordOrder;
+
+public Language(String name, int numSpeakers, String regionsSpoken,String wordOrder ){
+ this.name = name;
+ this.numSpeakers = numSpeakers;
+ this.regionsSpoken = regionsSpoken;
+ this.wordOrder = wordOrder;
+}
+
+public void getInfo(){
+System.out.println(this.name + " is spoken by " + this.numSpeakers + " people mainly in " + this.regionsSpoken + ".");
+System.out.println("The language follows the word order:"+ this.wordOrder );
+}
+
+public static void main(String[] args){
+Language Hindi = new Language("Hindi",1200000000,"Asia","subject-verb-object");
+Hindi.getInfo();
+
+System.out.println(" ");
+
+Mayan Yucatec = new Mayan("Yucatec",812633);
+Yucatec.getInfo();
+
+System.out.println(" ");
+SinoTibetan MandChinese = new SinoTibetan("Mandarin Chinese", 4500000);
+MandChinese.getInfo();
+
+System.out.println(" ");
+SinoTibetan Burma = new SinoTibetan("Burmese", 2500000);
+Burma.getInfo();
+ }
+}
